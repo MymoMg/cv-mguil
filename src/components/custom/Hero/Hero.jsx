3b82fade9ecfa9406e299833f31fab14529ee2d7
MymoMg <mmguil68@gmail.com>
2025-08-28 18:20:28 +0200

    update hero

--- src/components/custom/Hero/Hero.jsx
+++ src/components/custom/Hero/Hero.jsx
@@ -12,7 +12,7 @@ return (
                 <h2> MGUIL &nbsp; Meriem </h2>
                 <h1> Étudiante en Développement Web</h1>
                 <p> Après un parcours en web design et stratégie de communication, j’ai fait le choix de me reconvertir dans le développement web pour donner une nouvelle dimension à ma créativité mais surtout pour pouvoir produire mes créations web.
-                Actuellement en formation CDA - Concepteur Développeur d’Applications, je suis actuellement à la recherche d’un stage pour continuer à apprendre et monter en compétences. </p>
+                Actuellement en ..., je suis à la recherche d’une alernance pour continuer à apprendre et à monter en compétences. </p>
                 </div>
             <div className={styles.buttonContainer}>
                 <Button href="/assets/mg-meriem-cv.pdf" target="_blank">  Mon CV en PDF ›  </Button>
